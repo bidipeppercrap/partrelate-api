5f70b9ea7e58c2452c2d70b4faa34bd083ffc8ee
Fransisco Wijaya <bidipeppercrap@outlook.com>
2023-11-14 10:44:38 +0700

    sugar

--- db/schema/part_to_vehicle_part.ts
+++ db/schema/part_to_vehicle_part.ts
@@ -1,8 +1,11 @@
-import { primaryKey, integer, sqliteTable } from 'drizzle-orm/sqlite-core'
+import { integer, text, sqliteTable } from 'drizzle-orm/sqlite-core'
+import { vehicleParts } from './vehicle_parts'
+import { parts } from './parts'
 
 export const vehiclePartToVehicle = sqliteTable('part_to_vehicle_part', {
-    vehiclePartId: integer('vehicle_part_id'),
-    partId: integer('part_id'),
-}, (table) => ({
-    pk: primaryKey({ columns: [table.vehiclePartId, table.partId] })
-}))
+    id: integer('id').primaryKey(),
+    description: text('description'), // used to describe the usage of the part in the vehicle e.g. Left: 6201 2RS / Right: 6301 2RS
+    quantity: integer('quantity'),
+    vehiclePartId: integer('vehicle_part_id').references(() => vehicleParts.id),
+    partId: integer('part_id').references(() => parts.id),
+})
